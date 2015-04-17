10caec15cc35accbf1536050f5aa9ba9130b671d
Renato Coutinho <renato.coutinho@gmail.com>
2015-04-17 01:16:36 -0300

    Nova versão do post sobre a seca de 13/14, com plot das vazões.

--- _src/analise_pluviometria.R
+++ _src/analise_pluviometria.R
@@ -4,7 +4,7 @@ library(reshape)
 raw.ph <- read.csv("../data/pluv_hist_cantareira.csv", as.is=TRUE)
 ph <- zoo(raw.ph[,2:8], raw.ph[,1])
 ph.anual <- rowMeans(ph, na.rm=T)
-ph.bienal <- anual[-length(anual)] + anual[-1]
+ph.bienal <- ph.anual[-length(ph.anual)] + ph.anual[-1]
 
 ## vazoes
 ## leitura dados de referncai para outorga
