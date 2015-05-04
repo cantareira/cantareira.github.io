4183388648e370a6103862b81e2cca62e6a7697e
Renato Coutinho <renato.coutinho@gmail.com>
2015-05-04 02:17:25 -0300

    Adicionando script de update e copiando scrapers. Corrige um typo nos dados.

---
+++ _src/auto-update.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash
+
+ROOT=..
+hoje=`date +"%Y-%m-%d"`
+hoje2=`date +"%Y%m%d"`
+ontem=`date -d "yesterday" +"%Y-%m-%d"`
+commit=1
+
+pushd $ROOT
+
+wget "http://site.sabesp.com.br/site/uploads/file/boletim/boletim_mananciais.pdf"
+diff -q "boletim_mananciais.pdf" "boletins/boletim_mananciais_${ontem}.pdf"
+if [ $? != 0 ]; then
+    echo "** boletim dos mananciais parece atualizado **"
+    python _src/boletim_scraper.py boletim_mananciais.pdf data/dados.csv data/data_ocr_cor2.csv
+    if [ $? = 0 ]; then
+        mv boletim_mananciais.pdf "boletins/boletim_mananciais_${hoje}.pdf"
+        git add "boletins/boletim_mananciais_${hoje}.pdf" data/dados.csv data/data_ocr_cor2.csv
+        commit=0
+    else
+        rm boletim_mananciais.pdf
+    fi
+else
+    rm "boletim_mananciais.pdf"
+    echo "** boletim dos mananciais ainda não foi atualizado **"
+fi
+
+wget "http://www.sspcj.org.br/images/downloads/SSPCJ_boletimDiario_${hoje2}.pdf"
+if [ $? = 0 ]; then
+    echo "** boletim SSPCJ parece atualizado **"
+    # TODO: pdf_scraper não lida bem com boletins sem chuva
+    python _src/pdf_scraper.py "SSPCJ_boletimDiario_${hoje2}.pdf" data/previsoes_boletins_pcj.csv
+    if [ $? = 0 ]; then
+        rm "SSPCJ_boletimDiario_${hoje2}.pdf"
+        git add data/previsoes_boletins_pcj.csv
+        commit=0
+    fi
+else
+    echo "** boletim SSPCJ ainda não foi atualizado **"
+fi
+
+if [ "$commit" = 0 ]; then
+    echo "Cheque os dados antes de prosseguir. Continuar? (s/n)?"
+    read check
+    if [ ${check:0:1} = "s" ]; then
+        # update.R assume que estamos em _src
+        cd _src
+        R --no-save < update.R
+        if [ $? = 0 ]; then
+            cd ..
+            # "commit -a" é perigoso, lista arquivos individualmente
+            git add projecoes-${hoje}.html _includes/lista_projecoes.html dados.html dados_metadata.html data/dados_de_trabalho.csv data_ocr_cor2_metadata.html historico.html index.html planilha_de_trabalho_metadata.html sitemap.xml 
+            git commit -m "[auto] Novos dados e projeção."
+            # TODO: not yet!
+            #git push
+        else
+            cd ..
+        fi
+    fi
+else
+    echo "** Nenhum dado novo, nenhuma projeção realizada. **"
+fi
+
+popd
---
+++ _src/pdf_scraper.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# este script depende de pdftotext do sistema, além das bibliotecas em python abaixo
+import sys
+import subprocess
+import re
+import pycurl
+import os
+import datetime
+import calendar
+from dateutil import parser
+
+def daterange(start_date, end_date):
+    '''Returns range of dates between start and end, inclusive.'''
+    for n in range(int ((end_date - start_date).days) + 1):
+        yield start_date + datetime.timedelta(n)
+
+meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
+         'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+def translate_month(s):
+    for i in range(12):
+        s = s.replace(meses[i], calendar.month_name[i+1])
+    return s
+
+class PDF_Processor(object):
+    def __init__(self, urlformat, targets):
+        self.urlformat = urlformat
+        self.targets = targets
+
+    def scrape_pdf(self, arquivo, NA=''):
+        text = subprocess.Popen(["pdftotext", "-q", arquivo, "-"], stdout=subprocess.PIPE).communicate()[0]
+        text = text.decode('utf-8') # system-dependent?
+        text = text.replace("\n", ' ')
+        r = []
+        for target, pattern, transform in self.targets:
+            v = re.search(pattern, text)
+            if v:
+                r.append(transform(v.groups()))
+            else:
+                r.append(NA)
+        return r
+
+    def get_file(self, address):
+        # As long as the file is opened in binary mode, both Python 2 and Python 3
+        # can write response body to it without decoding.
+        fname = address.split('/')[-1]
+        with open(fname, 'wb') as f:
+            c = pycurl.Curl()
+            c.setopt(c.URL, address)
+            c.setopt(c.WRITEDATA, f)
+            c.perform()
+            c.close()
+    
+        return fname
+
+    def process_date(self, date):
+        f = self.get_file(self.urlformat.format(year=date.year,
+            month=date.month, day=date.day))
+        r = self.scrape_pdf(f)
+        os.remove(f)
+        return r
+
+    def process_daterange(self, start, end):
+        r = []
+        dr = list(daterange(start, end))
+        for date in dr:
+            r.append([date] + self.process_date(date))
+            print('.', end='')
+        return r
+
+# retira do boletim diário da ANA as vazões afluentes e defluentes do SE e
+# Paiva Castro, e QESI
+# status: OK! Falta comparar e manter uma tabela atualizada.
+patterns = ['Qaflu-SE = (\d+),(\d+)',
+            'Qdeflu-SE = (\d+),(\d+)m',
+            'Qaflu-PC = (\d+),(\d+)m',
+            'Qdeflu-PC = (\d+),(\d+)m',
+            'Q-T5 = (\d+),(\d+)m',
+            'Q-EESI = (\d+),(\d+)m']
+def transform(v):
+    return int(v[0]) + int(v[1])/100.
+targets = [(p.split(' ')[0], p, transform) for p in patterns]
+url = ("http://arquivos.ana.gov.br/saladesituacao/BoletinsDiarios/"
+       "DivulgacaoSiteSabesp_{day:d}-{month:d}-{year:d}.pdf")
+vazoes_ANA = PDF_Processor(url, tuple(targets))
+
+# retira do boletim diário do SSPCJ as previsões de chuvas de 5 dias
+# status: parece funcionar, ainda não é (e talvez nunca será) robusto
+url = ("http://www.sspcj.org.br/images/downloads/SSPCJ_boletimDiario_"
+       "{year:d}{month:02d}{day:02d}.pdf")
+def transform(v):
+    v = list(v)
+    v[0] = parser.parse(translate_month(v[0]), fuzzy=True)
+    v[1] = parser.parse(translate_month(v[1]), fuzzy=True)
+    v[2] = int(v[2])
+    v[3] = int(v[3])
+    return v
+targets = [('prev', ('A +previsão +de +chuvas +entre +os +dias +(.+) +a +(.+)'
+           ' +é +de +acumulados +de +(\d+) +a +(\d+) +mm +nas +Bacias +PCJ.'),
+           transform)]
+previsao_chuva_SABESP = PDF_Processor(url, tuple(targets))
+
+def pline(p):
+    return '"%s","%s",%d,%d\n' % (p[0].strftime('%Y-%m-%d'),
+            p[1].strftime('%Y-%m-%d'), p[2], p[3])
+
+if __name__ == '__main__':
+    p = previsao_chuva_SABESP.scrape_pdf(sys.argv[1])[0]
+    if len(sys.argv) > 2:
+        with open(sys.argv[2], "a") as outfile:
+            outfile.write(pline(p))
+    else:
+        print(pline(p))
+
